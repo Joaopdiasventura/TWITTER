ceac05253447448863adc1284d071ced2916a639
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2024-01-16 18:15:20 -0300

    testando o deploy

--- BACKEND/src/repositories/create-post/createPost.ts
+++ BACKEND/src/repositories/create-post/createPost.ts
@@ -6,10 +6,10 @@ export class CreatePostRepository implements ICreatePostRepository {
     async createPost(params: CreatePostParams): Promise<Post> {
         const post = await prisma.post.create({
             data: {
-                title: params.title,  // Use title from params
+                title: params.title,
                 content: params.content,
-                creator: params.creatorUser.email,  // Assuming creator is User's email
-                createdat: new Date(),  // Correct way to get current date and time
+                creator: params.creatorUser.email,
+                createdat: new Date(),
             },
         });
 
